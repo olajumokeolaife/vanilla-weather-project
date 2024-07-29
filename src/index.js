e181a68120ad6d6ceba428c6ac27f915b23d7f12
olajumokeolaife <olaifeolajumoke@gmail.com>
2024-07-29 16:36:22 +0100

    worked on the forecast section

--- src/index.js
+++ src/index.js
@@ -59,8 +59,33 @@ function handleSearchSubmit(event) {
   // this will make whatever city we type in the search bar to be the same as what shows in the city value
 }
 
+//FORECAST
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+          <div class="weather-forecast-icon">🌤️</div>
+            <div class="weather-forecast-temperatures">
+              <div class="weather-forecast-temperature">
+                <strong>15º</strong>
+              </div>
+              <div class="weather-forecast-temperature">9º</div>
+            </div>
+      </div>`;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchformElement = document.querySelector("#search-form");
 searchformElement.addEventListener("submit", handleSearchSubmit);
 
 //this city here is the default city that will show when we load the page the 1st time
 searchCity("Paris");
+displayForecast();
